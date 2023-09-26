a20fd225dfff22f61bf447445fabcd41508f3cb7
monarxxjain <jainmonark1000@gmail.com>
2023-09-27 02:48:45 +0530

    Data.js created

--- src/PDFComponents/PDFFile.js
+++ src/PDFComponents/PDFFile.js
@@ -6,6 +6,7 @@ import Footer from './intoductionPage/Footer';
 import PricingDetails from './intoductionPage/PricingDetails';
 import Customer from './common/Customer';
 import Disclaimer from './common/Disclaimer';
+import { page1, page2, notes, tableRows, tableColumns } from '../Data/data';
 
 const styles = StyleSheet.create({
   page: {
@@ -131,116 +132,10 @@ const styles = StyleSheet.create({
   disclaimer2: {marginBottom: "2px"},
 });
 
-const page2= {
-  table1: {
-    customerName: "Monark Jain",
-    effDate: "01 Aug 2023",
-    expDate: "31 Aug 2023"
-  },
-  table2: {
-    iata_icao: "LBG /LFPB",
-    quote: "23454",
-    quoteType: "Scheduled"
-  },
-  table3: {
-    airport: "Paris - Le Bourget Airport",
-    quoteDate: "11 Aug 2014",
-    upliftDate: "11 Aug 2014"
-  },
-  table4: {
-    city_state: "11 Aug 2014",
-    quantity: "1000",
-    fuelType: "Jet Fuel, Jet A-1"
-  },
-  table5: {
-    country: "France",
-    fightType: "Commercial/International",
-    deliveryMethod: "pipeline"
-  },
-
-
-  table6: {
-    priceIndexBasis: ".",
-    pricingCycle: ".",
-    intoPLaneAgent: "GCAB Air Total"
-  },
-
-  table7: {
-    indexPriceAmount: ".",
-    differential: ".",
-    fuelPrice: "4.328956",
-    intoPlaneFee: "0.000000000",
-
-
-    perUOMFee: "0.045210",
-    taxes: "0.000000",
-    totalPricePerUOM: "$4.374166",
-    curr_uom: "USD/USG"
-  },
-
-  table8: {
-    intoPlaneFeeDetail: "N/A",
-
-    perFlightFeeDetail: "N/A"
-  },
- 
-  table9: {
-    feeDetailPerUOM: {
-      "Sagess" : "0.045210",
-    },
-
-    taxDetail: "N/A"
-  }
-}
-
-const tableColumns = ["IATA", "ICAO" ," DOM/NTL", "UOM", "CUR", "DEL METHOD", "FUEL PRICE", "INTO PLANE", "OTH FEES" , "TAXES", "TOT PRICE", "P F FEE"]
-const tableRows = [
-  {
-    name: 1,
-    values: ["1","2","3","4","5","6","7","8","9","10","11","12"]
-  },
-  {
-    name: 2,
-    values: ["1","2","3","4","5","6","7","8","9","10","11","12"]
-  }
-]
-
-const notes=["note1", "Note 2" ,"Note 3","Note 4","Note5"]
-const page1 = {
-  companyData: {
-    companyLogo: "/download.png",
-    companyName: "Fiverr",
-    address: "This is address 1",
-    companyContactDetails: {
-      phoneNo: "(001) 1234 567689",
-      fax: "(101) 123-45678",
-      hourDispatch: "(001) 123-456-789",
-      email: "email@email.com"
-    }
-  },
-
-  customerData: {
-    customerName: "Monark Jain",
-    creditLine: "150,000.00",
-    terms: "Net 14 Days",
-    estimateDate: "11 Aug 2023",
-    quote: "23425",
-    totalPages: 4
-  },
-
-  salesPersonData: {
-    salesPersonName: "Monark Jain",
-    phoneNo_O: "3948690900",
-    phoneNo_M: "8939865893869",
-    email:"oenrgo@gmail.com"
-
-  }
-}
-
-const PDFFile = ({data}) => (
+const PDFFile = () => (
   <Document>
     
-    <Page size="A4">
+    <Page>
       <Header page1={page1} styles={styles}/>
        
       <Text style={[styles.page, styles.toText]}>To: </Text>
